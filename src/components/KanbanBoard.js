73862848bb6971cbc4e81c047d55860b44f1158b
Asrani-Aman <amanasrani1712@gmail.com>
2024-09-13 12:36:56 +0530

    first commit

---
+++ src/components/KanbanBoard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Column from './Column';
+import { groupTickets, sortTickets } from '../utils/ticketUtils';
+import '../styles/KanbanBoard.css';
+
+function KanbanBoard({ tickets, users, grouping, sorting }) {
+  const groupedTickets = groupTickets(tickets, grouping, users);
+  const sortedGroupedTickets = sortTickets(groupedTickets, sorting);
+
+  return (
+    <div className="kanban-board">
+      {Object.entries(sortedGroupedTickets).map(([groupName, groupTickets]) => (
+        console.log(groupName),
+        <Column
+          key={groupName}
+          title={groupName}
+          tickets={groupTickets}
+          users={users}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default KanbanBoard;
